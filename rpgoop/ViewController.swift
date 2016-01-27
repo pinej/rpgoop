0df211bea6bd5899b75c57a326c3cdb6c0f60fd0
pinej <piotr.nejman@gmail.com>
2016-01-27 22:43:59 +0100

    Initial Commit

---
+++ rpgoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgoop
+//
+//  Created by Piotr Nejman on 27.01.2016.
+//  Copyright © 2016 pinej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
